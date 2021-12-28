1c5e342104ce2f565c584ce2ae5356c60ffa4607
Quinn Shanahan <q.shanahan@gmail.com>
2021-12-27 22:26:55 -0500

    add subdomain

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -44,8 +44,8 @@ resource "aws_route53_record" "staging" {
   type    = "A"
 
   alias {
-    name                   = data.aws_lb.main.dns_name
-    zone_id                = data.aws_lb.main.zone_id
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
     evaluate_target_health = true
   }
 }
