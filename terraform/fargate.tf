ca099f1b7c4432669b481844a28b75cbcc52de18
Quinn Shanahan <q.shanahan@gmail.com>
2021-12-27 21:52:00 -0500

    add remainder of resources for lb and service

--- terraform/fargate.tf
+++ terraform/fargate.tf
@@ -1,16 +1,44 @@
-# We'll eventually want a place to put our logs.
-resource "aws_cloudwatch_log_group" "sun_api" {
+# credit to https://section411.com/2019/07/hello-world/ for
+# guide on creating these resources
+
+resource "aws_ecs_service" "graphql_api" {
+  name            = "politicker-graphql-api"
+  task_definition = aws_ecs_task_definition.graphql_api.arn
+  cluster         = aws_ecs_cluster.graphql_api.id
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    assign_public_ip = false
+
+    security_groups = [
+      aws_security_group.egress_all.id,
+      aws_security_group.ingress_api.id,
+    ]
+
+    subnets = [
+      aws_subnet.private_d.id,
+      aws_subnet.private_e.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.graphql_api.arn
+    container_name   = "politicker-graphql-api"
+    container_port   = "3000"
+  }
+}
+
+resource "aws_ecs_cluster" "graphql_api" {
+  name = "politicker-graphql-api"
+}
+
+resource "aws_cloudwatch_log_group" "graphql_api" {
   name = "/ecs/politicker-graphql-api"
 }
 
-# Here's our task definition, which defines the task that will be running to provide
-# our service. The idea here is that if the service decides it needs more capacity,
-# this task definition provides a perfect blueprint for building an identical container.
-#
-# If you're using your own image, use the path to your image instead of mine,
-# i.e. `<your_dockerhub_username>/sun-api:latest`.
 resource "aws_ecs_task_definition" "graphql_api" {
-  family = "politicker-graphql-api"
+  family             = "politicker-graphql-api"
+  execution_role_arn = aws_iam_role.graphql_api_task_execution_role.arn
 
   container_definitions = jsonencode(
     [
@@ -26,7 +54,7 @@ resource "aws_ecs_task_definition" "graphql_api" {
           "logDriver" : "awslogs",
           "options" : {
             "awslogs-region" : "us-east-2",
-            "awslogs-group" : aws_cloudwatch_log_group.sun_api.name,
+            "awslogs-group" : aws_cloudwatch_log_group.graphql_api.name,
             "awslogs-stream-prefix" : "ecs"
           }
         }
