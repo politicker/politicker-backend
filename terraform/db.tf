b760d3f8ea0e1a00d95f48cab8e8971ecadb2be3
Quinn Shanahan <q.shanahan@gmail.com>
2021-12-29 18:51:57 -0500

    typo

--- terraform/db.tf
+++ terraform/db.tf
@@ -1,4 +1,4 @@
-resource "aws_db_subnet_group" "default" {
+resource "aws_db_subnet_group" "main" {
   name       = "politicker-db-subnet"
   subnet_ids = ["${aws_subnet.private_a.id}", "${aws_subnet.private_b.id}"]
 
@@ -22,4 +22,5 @@ resource "aws_db_instance" "main" {
   username               = "politicker"
   password               = random_password.password.result
   vpc_security_group_ids = [aws_security_group.ingress_db.id]
+  db_subnet_group_name   = aws_db_subnet_group.main.name
 }
